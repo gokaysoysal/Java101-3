5fe9033ce5131c00c1fdcd26526a248fdcb77957
Gökay Soysal <agokaysoysal@gmail.com>
2022-04-07 21:59:57 +0300

    KDV Hesaplama

---
+++ src/KDV.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class KDV {
+    public static void main(String[] args) {
+
+        double kdv = 0.18, fiyat, tutar, vergi;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Fiyati giriniz: ");
+        fiyat = input.nextDouble();
+
+        vergi = fiyat * kdv;
+
+        tutar = (fiyat * kdv) + fiyat;
+
+        System.out.println("Fiyat: " + fiyat);
+        System.out.println("KDV Oranı: " + kdv);
+        System.out.println("KDV Tutarı: " + vergi);
+        System.out.print("Vergiler Dahil Tutar: " + tutar);
+
+    }
+}
