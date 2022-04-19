9b1fba55a74f41dc354733a288820a2e04f6e136
Gökay Soysal <agokaysoysal@gmail.com>
2022-04-19 21:46:13 +0300

    shine bright like a diamond

--- src/WrittenInTheStarzzz.java
+++ src/WrittenInTheStarzzz.java
@@ -17,6 +17,15 @@ public class WrittenInTheStarzzz {
 
             System.out.println();
         }
+        for (int i=(n-1);i>=0;i--){
+            for (int k = 0; k<(n-i); k++){
+                System.out.print(" ");
+            }
+            for (int j=0;j<(2 * i - 1); j++){
+                System.out.print("*");
+            }
+            System.out.println(" ");
+        }
 
     }
 }
